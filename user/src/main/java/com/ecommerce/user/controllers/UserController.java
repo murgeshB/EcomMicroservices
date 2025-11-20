fa3779a220d67e2f602d9e4a48a28895179b9e77
murgeshB <murgeshbiradar133@gmail.com>
2025-11-20 23:23:30 +0530

    UserController.java changes

--- user/src/main/java/com/ecommerce/user/controllers/UserController.java
+++ user/src/main/java/com/ecommerce/user/controllers/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
+//User Controller - User Api
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
